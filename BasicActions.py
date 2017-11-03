f350ddb2a5cc196d030783695b3d4e7be86bde9f
zender991 <zender991@gmail.com>
2017-11-03 17:47:32 +0200

    FB login

---
+++ BasicActions.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+import unittest
+
+
+class LoginTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome("C:\Libs\chromedriver.exe")
+        self.driver.get("https://www.facebook.com/")
+
+
+    def test_login(self):
+        driver = self.driver
+        facebook_username = 'pittbulltest@yandex.ru'
+        facebook_pass = 'pitttest'
+        email_field_id = 'email'
+        pass_field_id = 'pass'
+        login_button_xpath = "//label[@id='loginbutton']"
+
+
+        email_field_element = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(email_field_id))
+        pass_field_element = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(pass_field_id))
+        login_button_element = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(login_button_xpath))
+
+        email_field_element.clear()
+        email_field_element.send_keys(facebook_username)
+        pass_field_element.clear()
+        pass_field_element.send_keys(facebook_pass)
+        login_button_element.click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
