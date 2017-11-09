10ed6d1873b68f94b568d0b7cf4de8bdc36756d7
zender991 <zender991@gmail.com>
2017-11-09 21:20:09 +0300

    add handler

--- test_zp.py
+++ test_zp.py
@@ -43,10 +43,27 @@ class LoginTest(unittest.TestCase):
         zp_app_link = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_link_text(zp_app_title))
         zp_app_link.click()
 
+        #windowhanles = driver.window_handles[1]
+        #driver.SwitchTo().Window(driver.WindowHandles.Last())
+
+        driver.switch_to.window(driver.window_handles[-1])
+        #driver.switch_to_window(driver.window_handles[-1])
+
         #pages_app_title = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_xpath("//h1[text()='Pages']"))
-        cr_page = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_link_text("Create Page"))
+        #cr_page = WebDriverWait(driver, 30).until(lambda driver: driver.find_element_by_xpath("//a[text()='Create Page']/@href"))
+
+        cr_page = WebDriverWait(driver, 30).until(lambda driver: driver.find_element_by_xpath("(//a[@href='/pages/templates?shop=shopifypages-shop1.myshopify.com'])[1]"))
+        #cr_page = WebDriverWait(driver, 30).until(lambda driver: driver.find_element_by_xpath("(//a[contains(@title,'Create Page')])[1]"))
         cr_page.click()
 
+        create_template_button = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_xpath("(//button[text()='Use Template'])[5]"))
+        create_template_button.click()
+
+        page_title_field = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_id("page_create_title"))
+        page_title_field.send_keys("qqqqwwwww")
+
+        create_page_button = WebDriverWait(driver, 20).until(lambda driver: driver.find_element_by_xpath("//button[text()='Create']"))
+        create_page_button.click()
 
 
 
